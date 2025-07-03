83480ac08a8558c8312a48c956a0ac43a05d074b
joeeddy <jdeddy1976@gmail.com>
2025-07-03 15:38:13 -0400

    Create agent.py
    Add agent.py

---
+++ src/agent.py
@@ -0,0 +1,7 @@
+class Agent:
+    def __init__(self, name):
+        self.name = name
+
+    def act(self, observation):
+        # Placeholder action logic
+        return "action_based_on_" + str(observation)
